d5b9ab282d2288e5902bb142124b88eff3715fa6
gustavo1245 <gustavo.soto.morales@alumnos.sip.cl>
2024-11-27 11:44:54 -0300

    actualizacion  js 3

---
+++ script.js
@@ -0,0 +1,56 @@
+function alerta() {
+    alert("Inicio de Sesion Exitoso");
+}
+
+function cambiarestado(boton) {
+    boton.textContent = "No Disponible";
+    boton.style.backgroundColor = "red";
+    boton.style.color = "white";
+}
+
+function cambiarTitulo(genero) {
+    const titulo = document.getElementById("titulo");
+    titulo.textContent = genero;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
